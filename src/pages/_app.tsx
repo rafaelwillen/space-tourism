3a7c93e5a2e0a82f1c9e3a687a28ac678154a1ea
Rafael Willen <rafaelpadre20@gmail.com>
2023-04-04 15:46:17 +0100

    Add app theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+
+import type { AppProps } from "next/app";
+import { Barlow, Barlow_Condensed, Bellefair } from "next/font/google";
+
+const headingFont = Bellefair({
+  variable: "--font-bellefair",
+  subsets: ["latin"],
+  weight: "400",
+});
+const bodyCondensedFont = Barlow_Condensed({
+  variable: "--font-barlow-condensed",
+  subsets: ["latin"],
+  weight: "400",
+});
+const bodyFont = Barlow({
+  variable: "--font-barlow",
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main
+      className={`${headingFont.variable} ${bodyCondensedFont.variable} ${bodyFont.variable} font-body text-body`}
+    >
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,50 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      mobile: "375px",
+      table: "768px",
+      desktop: "1440px",
+    },
+    colors: {
+      primary: "#0b0d17",
+      secondary: "#d0d6f9",
+      white: "#fff",
+    },
+    fontFamily: {
+      heading: ["var(--font-bellefair)", ...fontFamily.sans],
+      "body-condensed": ["var(--font-barlow-condensed)", ...fontFamily.sans],
+      body: ["var(--font-barlow)", ...fontFamily.sans],
+    },
+    fontSize: {
+      "heading-1": "150px",
+      "heading-2": "100px",
+      "heading-3": "56px",
+      "heading-4": "32px",
+      "heading-5": [
+        "28px",
+        {
+          letterSpacing: "4.75px",
+        },
+      ],
+      "subheading-1": "28px",
+      "subheading-2": [
+        "14px",
+        {
+          letterSpacing: "2.35px",
+        },
+      ],
+      "nav-text": [
+        "16px",
+        {
+          letterSpacing: "2.7px",
+        },
+      ],
+      body: "18px",
+    },
   },
   plugins: [],
 };
