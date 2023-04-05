475117e9d071c33730a7501c7d8c191c15039b0b
Rafael Willen <rafaelpadre20@gmail.com>
2023-04-05 10:15:10 +0100

    Add the header for the tablet view

--- src/components/layouts/Container.tsx
+++ src/components/layouts/Container.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
 
 export default function Container({ children }: PropsWithChildren) {
-  return <div className="p-6">{children}</div>;
+  return <div className="p-6 tablet:p-0">{children}</div>;
 }
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
   const { pathname } = useRouter();
 
   return (
-    <header className="text-nav-text font-body-condensed flex justify-between items-center">
+    <header className="text-nav-text font-body-condensed flex justify-between items-center tablet:pl-10">
       <Image src={LogoSVG} alt="Space Tourism Logo" className="w-12 h-12" />
       <button
         onClick={() => setIsMenuVisible(true)}
@@ -23,59 +23,69 @@ export default function Header() {
       </button>
 
       <nav
-        className={`${
+        className={`
+        tablet:block relative w-auto mobile-menu
+        ${
           isMenuVisible
             ? "block absolute top-0 bottom-0 right-0 w-[254px] mobile-menu font-body-condensed text-body"
             : "hidden"
         }`}
       >
-        <div className="relative z-10 pt-8 pl-8 flex flex-col space-y-16">
+        <div className="relative z-10 pt-8 pl-8 flex flex-col space-y-16 tablet:flex-row tablet:p-0 tablet:space-y-0">
           <button
             type="button"
             onClick={() => setIsMenuVisible(false)}
-            className="flex self-end mr-6"
+            className="tablet:hidden flex self-end mr-6"
           >
             <Image src={CloseSVG} alt="Close Button" />
           </button>
-          <ul className="flex flex-col gap-8">
+          <ul className="flex flex-col gap-8 tablet:flex-row tablet:px-12 tablet:gap-9">
             <li>
               <Link
-                className={`flex gap-3 py-2 ${
-                  routes.home === pathname ? "border-r-4" : ""
+                className={`flex gap-3 py-2 tablet:py-10 ${
+                  routes.home === pathname
+                    ? "border-r-4 tablet:border-b-4 tablet:border-r-0"
+                    : ""
                 } `}
                 href={routes.home}
               >
-                <span className="font-bold">00</span>Home
+                <span className="font-bold tablet:hidden">00</span>Home
               </Link>
             </li>
             <li>
               <Link
-                className={`flex gap-3 py-2 ${
-                  routes.destination === pathname ? "border-r-4" : ""
+                className={`flex gap-3 py-2 tablet:py-10${
+                  routes.destination === pathname
+                    ? "border-r-4 tablet:border-r-0 "
+                    : ""
                 } `}
                 href={routes.destination}
               >
-                <span className="font-bold">01</span>Destination
+                <span className="font-bold tablet:hidden">01</span>Destination
               </Link>
             </li>
             <li>
               <Link
-                className={`flex gap-3 py-2 ${
-                  routes.crew === pathname ? "border-r-4" : ""
+                className={`flex gap-3 py-2 tablet:py-10${
+                  routes.crew === pathname
+                    ? "border-r-4 tablet:border-r-0 "
+                    : ""
                 } `}
                 href={routes.crew}
               >
-                <span className="font-bold">02</span>Crew
+                <span className="font-bold tablet:hidden">02</span>Crew
               </Link>
             </li>
             <li>
               <Link
-                className={`flex gap-3 py-2 ${
-                  routes.technology === pathname ? "border-r-4" : ""
+                className={`flex gap-3 py-2 tablet:py-10 ${
+                  routes.technology === pathname
+                    ? "border-r-4 tablet:border-r-0 "
+                    : ""
                 } `}
                 href={routes.technology}
               >
-                <span className="font-bold">03</span>Technology
+                <span className="font-bold tablet:hidden">03</span>Technology
               </Link>
             </li>
           </ul>
