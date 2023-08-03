ab56c6e284ddd231068fa3c3dd88bd4931cc7db4
Rafael Willen <rafaelpadre20@gmail.com>
2023-08-03 12:08:20 +0100

    Setup theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/components/**/*.tsx", "./src/app/**/*.tsx"],
   theme: {
+    colors: {
+      black: "#0b0d17",
+      primary: "#d0d6f9",
+      white: "#fff",
+    },
+    fontFamily: {
+      heading: ["var(--heading)"],
+      body: ["var(--body)"],
+    },
+    screens: {
+      mobile: "375px",
+      tablet: "768px",
+      desktop: "1440px",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize: {
+        "heading-1": "9.375rem",
+        "heading-2": "6.25rem",
+        "heading-3": "3.5rem",
+        "heading-4": "2rem",
+        "heading-5": "1.75rem",
+        "subheading-1": "1.75rem",
+        "subheading-2": "0.875rem",
+        nav: "1rem",
+        body: "1.125rem",
+      },
+      letterSpacing: {
+        "heading-5": "4.75px",
+        "subheading-2": "2.35px",
+        nav: "2.7px",
       },
     },
   },
   plugins: [],
-}
+};
